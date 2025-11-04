9d2256be0eafdc525c0722fc719eb27a3045be65
ardyutama <ardy.putrautama53@gmail.com>
2025-11-04 08:16:24 +0800

    draft: add heading section

--- app/components/sections/Projects.vue
+++ app/components/sections/Projects.vue
@@ -1,18 +1,48 @@
 <script lang="ts" setup>
-
+  import HeadingSection from '../ui/HeadingSection.vue';
 </script>
 
 <template>
-  <section class="projects-section">
-    
+  <section class="projects__section">
+    <div class="projects__container">
+      <div class="projects__content">
+      <HeadingSection>Things I’ve made</HeadingSection>
+      <div class="projects__caption">
+        Figma ipsum component variant main layer. Variant rotate layout auto fill arrow. Overflow rectangle thumbnail list distribute pixel underline component editor invite.
+      </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
 @use '~/assets/scss/global' as *;
 
-.projects-section {
-  background-color: $token-gray-light;
-  min-height: 100vh;
+.projects {
+  &__section {
+    background-color: $token-gray-light;
+    min-height: 100vh;
+  }
+
+  &__container {
+    padding: var(--padding-section-x) var(--padding-section-y);
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+  }
+
+  &__content {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: var(--gap-lg);
+    max-width: $breakpoint-sm;
+  }
+
+  &__caption {
+    font-family: var(--font-family-body);
+    font-weight: 600;
+  }
 }
 </style>
--- app/components/ui/HeadingSection.vue
+++ app/components/ui/HeadingSection.vue
@@ -1,13 +1,34 @@
-<template>
-  <div>
-    
-  </div>
-</template>
-
 <script lang="ts" setup>
 
 </script>
 
-<style>
+<template>
+  <div class="heading-section">
+    <span class="heading-section__text">
+      <slot />
+    </span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.heading-section {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 6px 0 0 var(--border-main);
+  background-color: var(--text-light);
+  padding: var(--padding-section-heading-y) var(--padding-btn-x);
+  
+  border-radius: var(--border-radius-pill);
+  border: 2px solid var(--border-main);
+  
+  &__text {
+    font-family: var(--font-family-heading);
+    font-size: var(--font-size-btn);
+    font-weight: 600;
+    color: var(--text-primary);
+  }
+}
 
 </style>
